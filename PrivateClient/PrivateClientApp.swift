e2b11c57a7807c835861ed47ca83eab44fc8338e
Tarun <me@tarun.uk>
2026-04-10 17:04:23 +0100

    Initial Commit

---
+++ PrivateClient/PrivateClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrivateClientApp.swift
+//  PrivateClient
+//
+//  Created by Tarun Pemmaraju on 10/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct PrivateClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
